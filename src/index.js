59ddb7b1f7e01bdc19c05fd96f5509cfe0172bd3
AKHILSUNNI <70759789+AKHILSUNNI@users.noreply.github.com>
2021-09-11 16:20:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@ import Dogapp from "./Dogapp"
 import "./app.css"
 
 ReactDOM.render(<Dogapp />,document.getElementById("root"))
-
-//all breed api - https://dog.ceo/api/breeds/list/all
-//all sub-breed api - https://dog.ceo/api/breed/hound/list
-// all breed image  - https://dog.ceo/api/breed/hound/images
-// all sub-breed image - https://dog.ceo/api/breed/hound/afghan/images
